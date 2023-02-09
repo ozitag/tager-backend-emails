a358eaacd18ab6e6f4220970d1fac4de20cf9350
Vital Ozierski <vital@ozerich.com>
2023-02-09 13:06:33 +0300

    1.10.11 -  replace Bulder with BuilderContract

--- src/Repositories/MailLogRepository.php
+++ src/Repositories/MailLogRepository.php
@@ -2,7 +2,7 @@
 
 namespace OZiTAG\Tager\Backend\Mail\Repositories;
 
-use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Contracts\Database\Eloquent\Builder;
 use OZiTAG\Tager\Backend\Core\Repositories\EloquentRepository;
 use OZiTAG\Tager\Backend\Core\Repositories\ISearchable;
 use OZiTAG\Tager\Backend\Mail\Enums\MailStatus;
